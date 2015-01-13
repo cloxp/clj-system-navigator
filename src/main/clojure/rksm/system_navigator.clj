4a198fad0d98be9b3ccdfbb7780b394af05d8c58
lively-web <unknown-user@lively-web.org>
2015-01-13 21:29:56 +0000

    renaming

--- src/main/clojure/rksm/system_navigator.clj
+++ src/main/clojure/rksm/system_navigator.clj
@@ -1,4 +1,5 @@
 (ns rksm.system-navigator
-  (:require (rksm.system-navigator search clojars completions json ns_internals namespaces)))
+  (:require (rksm.system-navigator search clojars completions json)
+            (rksm.system-navigator.ns internals filemapping)))
 
 ; the core module to pull in the rest for cloxp tools
