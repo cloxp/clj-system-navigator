4bdec0f8405966205659fe393da19da50eccd4c0
lively-web <unknown-user@lively-web.org>
2015-01-14 20:12:56 +0000

    experimenting with alternative source retrieval

--- src/main/clojure/rksm/system_navigator/ns/internals.clj
+++ src/main/clojure/rksm/system_navigator/ns/internals.clj
@@ -1,11 +1,71 @@
 (ns rksm.system-navigator.ns.internals
     (:require [clojure.data.json :as json]
-              [clojure.repl :as repl]))
+              [clojure.repl :as repl]
+              [clojure.tools.reader :as tr]
+              [rksm.system-navigator.ns.filemapping :refer (file-for-ns source-reader-for-ns)])
+    (:import (java.io LineNumberReader InputStreamReader PushbackReader)
+             (clojure.lang LineNumberingPushbackReader)
+           (clojure.lang RT)))
+
+
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+; source retrieval
+
+(defn- read-next-obj
+  "follows the reader while it core/reads an object and returns the string in
+  range of what was read"
+  [rdr]
+  (let [text (StringBuilder.) 
+        pbr (proxy [PushbackReader] [rdr]
+                   (read []
+                         (let [i (proxy-super read)]
+                           (if (> i -1)(.append text (char i)))
+                           i)))]
+    (if (= :unknown *read-eval*)
+      (throw (IllegalStateException. "Unable to read source while *read-eval* is :unknown."))
+      (tr/read (PushbackReader. pbr) false nil))
+    (str text)))
+
+(defn- read-entity-source
+  "goes forward in line numbering reader until line of entity is reached and
+  reads that as an object"
+  [{lrdr :lrdr sources :sources} meta-entity]
+  (or (if-let [line (:line meta-entity)]
+        (do
+          (dotimes [_ (dec (- line (.getLineNumber lrdr)))] (.readLine lrdr))
+          {:lrdr lrdr
+           :sources (conj sources
+                          (assoc meta-entity
+                                 :source
+                                 (read-next-obj lrdr)))}))
+      {:lrdr lrdr :sources sources}))
+
+(defn add-source-to-interns-with-reader
+  [rdr interns]
+  (let [source-data {:lrdr (LineNumberReader. rdr) :sources []}]
+    (or (some->> interns
+          (reduce read-entity-source source-data)
+          :sources)
+        interns)))
+
+(defn add-source-to-interns
+  [ns interns]
+  (if-let [rdr (source-reader-for-ns ns)]
+    (with-open [rdr rdr]
+      (add-source-to-interns-with-reader rdr interns))
+    interns))
+
+; (defn source-for-symbol [sym]
+;   (try
+;     (with-out-str (eval `(source-retrieval ~sym)))
+;     (catch Exception e (str "Error retrieving source: " e))))
 
 (defn source-for-symbol [sym]
-  (try
-    (with-out-str (eval `(repl/source ~sym)))
-    (catch Exception e (str "Error retrieving source: " e))))
+  (try (clojure.repl/source-fn sym)
+    (catch Exception e "")))
+
+; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+; namespace / meta data interface
 
 (defn protocol-info
   [intern-info]
@@ -13,22 +73,22 @@
     {:protocol (meta p)} {}))
 
 (defn intern-info
-  [intern-var]
-    (let [data (meta intern-var)]
-      (if-let [ns (:ns data)]
-        (let [name (:name data)
-              ns-name (.name ns)
-              tag (if-let [tag (:tag data)] (str tag))
-              source (source-for-symbol (symbol (format "%s/%s" ns-name name)))]
-            (merge data 
-                   {:ns ns-name :tag tag :source source}
-                   (protocol-info data))))))
+  [intern-meta]
+  (if-let [ns (:ns intern-meta)]
+    (let [name (:name intern-meta)
+          ns-name (.name ns)
+          tag (if-let [tag (:tag intern-meta)] (str tag))]
+      (merge intern-meta
+             {:ns ns-name :tag tag}
+             (protocol-info intern-meta)))))
 
 (defn namespace-info
   [ns]
   (->> (ns-interns ns)
-       (map #(-> % val intern-info))
-       (filter boolean)))
+    (map #(-> % val meta intern-info))
+    (sort-by :line)
+    (add-source-to-interns ns)
+    (filter boolean)))
 
 (defn symbol-info
   [ns sym]
@@ -39,7 +99,7 @@
   (cond
     (var? obj) (:name (meta obj))
     (or (string? obj) (symbol? obj) (keyword? obj)) (name obj)
-    :else (str obj))) 
+    :else (str obj)))
 
 (defn symbol-info->json
   [ns sym]
@@ -56,6 +116,8 @@
 
 (comment
   (source-for-symbol 'rksm.system-navigator.test.dummy-1/x)
+  (rksm.system-navigator.ns.internals/namespace-info->json 'rksm.system-navigator.search)
+  (namespace-info 'rksm.system-navigator.search)
   (namespace-info 'rksm.system-navigator.test.dummy-1)
   (namespace-info->json 'rksm.system-navigator.test.dummy-1)
   )
--- src/test/clojure/rksm/system_navigator/ns/internals_test.clj
+++ src/test/clojure/rksm/system_navigator/ns/internals_test.clj
@@ -5,16 +5,45 @@
             [rksm.system-navigator.test.dummy-1]))
 
 (deftest ns-internals
-
+  
   (testing "get source for intern"
-    (is (= "(def x 23)\n"
+    (is (= "(def x 23)"
            (source-for-symbol 'rksm.system-navigator.test.dummy-1/x))))
+  
+  (testing "extract meta entities from source"
+    
+    (testing "meta entities match source"
+      (is (= [{:source "(def x 23)" :column 1,:line 1}
+              {:source "(def y 24)" :column 1,:line 2}]
+             (let [entities [{:column 1,:line 1} {:column 1,:line 2}]
+                   source (java.io.StringReader. "(def x 23)\n(def y 24)\n")]
+               (add-source-to-interns-with-reader source entities)))))
+    
+    (testing "less meta entities than source"
+      (is (= [{:source "(def x 23)" :column 1,:line 1}
+              {:source "(def y 24)" :column 1,:line 6}]
+             (let [entities [{:column 1,:line 1} {:column 1,:line 6}]
+                   source (java.io.StringReader. "(def x 23)\n(def baz\n\n99)\n\n(def y 24)\n")]
+               (add-source-to-interns-with-reader source entities)))))
+    
+    (testing "more meta entities than source"
+      (is (= [{:source "(def x 23)" :column 1,:line 1}]
+             (let [entities [{:column 1,:line 1} {:column 1,:line 6}]
+                   source (java.io.StringReader. "(def x 23)")]
+               (add-source-to-interns-with-reader source entities)))))
+    
+    (testing "not entities in source"
+      "this might be kind of unexpected but the reader des not care bout lines"
+      (is (= [{:source "(def y 24)" :column 1,:line 2}]
+             (let [entities [{:column 1,:line 2} {:column 1,:line 6}]
+                   source (java.io.StringReader. "(def x 23)\n\n(def y 24)")]
+               (add-source-to-interns-with-reader source entities))))))
 
   (testing "namespace report"
     (let [expected [{:ns 'rksm.system-navigator.test.dummy-1,
                      :name 'x,
                      :file "rksm/system_navigator/test/dummy_1.clj",
-                     :source "(def x 23)\n",
+                     :source "(def x 23)",
                      :column 1,
                      :line 3,
                      :tag nil}]]
@@ -26,4 +55,21 @@
 (comment
   (run-tests 'rksm.system-navigator.ns.internals-test)
   (run-all-tests)
+  (ns-interns 'rksm.system-navigator.test.dummy-1)
+  (namespace-info 'rksm.system-navigator.test.dummy-1)
+  
+  (rksm.system-navigator.ns.filemapping/add-classpath "test-resources/dummy-2-test.jar")
+  (require 'rksm.system-navigator.test.dummy-1)
+  (require 'rksm.system-navigator.test.dummy-2)
+
+  (namespace-info 'rksm.system-navigator.test.dummy-2)
+  (require '[clj-stacktrace.repl :refer (pst)])
+  (require '[rksm:refer (pst)])
+  source-retrieval
+  pst
+  (source-retrieval 'rksm.system-navigator.test.dummy-1/x)
+  (source-for-symbol 'rksm.system-navigator.test.dummy-1/x)
+  (meta #'rksm.system-navigator.test.dummy-1/x)
+  (pst)
+  
   )
