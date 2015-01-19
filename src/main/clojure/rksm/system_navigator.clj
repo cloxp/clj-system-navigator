8f2fd35f8aea23ac3a2df2578fe85f1f51754a4f
Robert Krahn <robert.krahn@gmail.com>
2015-01-18 16:36:35 -0800

    adding sys browser to reqs

--- src/main/clojure/rksm/system_navigator.clj
+++ src/main/clojure/rksm/system_navigator.clj
@@ -1,5 +1,5 @@
 (ns rksm.system-navigator
-  (:require (rksm.system-navigator search clojars completions json)
+  (:require (rksm.system-navigator search clojars completions json system-browser)
             (rksm.system-navigator.ns internals filemapping)))
 
 ; the core module to pull in the rest for cloxp tools
