1812c4d652af343593a0d731d34cefa0503d8c99
Robert Krahn <robert.krahn@gmail.com>
2015-01-20 00:37:29 -0800

    no extra json ns

--- src/main/clojure/rksm/system_navigator.clj
+++ src/main/clojure/rksm/system_navigator.clj
@@ -1,5 +1,5 @@
 (ns rksm.system-navigator
-  (:require (rksm.system-navigator search clojars completions json system-browser)
+  (:require (rksm.system-navigator search clojars completions system-browser)
             (rksm.system-navigator.ns internals filemapping)))
 
 ; the core module to pull in the rest for cloxp tools
