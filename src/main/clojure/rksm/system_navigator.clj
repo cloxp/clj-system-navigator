7f285178a0ad01e97fe7af49e320cbfe1c2fb976
Robert Krahn <robert.krahn@gmail.com>
2015-02-14 03:18:49 -0800

    switching back, no nrepl handler, just using pure cloxp-repl eval

--- src/main/clojure/rksm/system_navigator.clj
+++ src/main/clojure/rksm/system_navigator.clj
@@ -2,4 +2,6 @@
   (:require (rksm.system-navigator search clojars completions system-browser dependencies)
             (rksm.system-navigator.ns internals filemapping)))
 
+(def ^{:dynamic true} *repl-source*)
+
 ; the core module to pull in the rest for cloxp tools
